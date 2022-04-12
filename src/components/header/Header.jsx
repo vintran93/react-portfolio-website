15bcad68e6b5fc6c79fe1b83450fd8e1a6661390
Vincent Tran <vincentuong@yahoo.com>
2022-04-11 22:56:43 -0500

    icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
+import HeaderSocials from './HeaderSocials'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+      
+
+      <h5>Hello my name is</h5>
+      <h1>Vincent Tran</h1>
+      <h5 className="text-light">Fullstack Developer</h5>
+      <div className="me">
+        <img src={ME} alt="me" />
+      </div>
+      <CTA />
+      
+
+      <HeaderSocials />
+      
+
+      <a href="#contact" className="scroll__down">Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
