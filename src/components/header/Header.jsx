5039d6f12ef9fb8ffd7643a4bf4b9c4229bc22ec
vincent <vincentuong@yahoo.com>
2022-09-28 17:00:51 -0500

    picture update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.jpg'
+import ME from '../../assets/Vietnam-Da-Lat.jpg'
 import HeaderSocials from './HeaderSocials'
 
 
@@ -9,21 +9,21 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-      
 
-      <h5>Hello my name is</h5>
-      <h1>Vincent Tran</h1>
-      <h5 className="text-light">Fullstack Developer</h5>
-      <div className="me">
-        <img src={ME} alt="me" />
-      </div>
-      <CTA />
-      
 
-      <HeaderSocials />
-      
+        <h5>Hello my name is</h5>
+        <h1>Vincent Tran</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <CTA />
 
-      <a href="#contact" className="scroll__down">Scroll Down</a>
+
+        <HeaderSocials />
+
+
+        <a href="#contact" className="scroll__down">Scroll Down</a>
       </div>
     </header>
   )
