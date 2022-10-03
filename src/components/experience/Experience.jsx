cdd9d50334dcd209901489762dbc6ffcdd78229c
vincent <vincentuong@yahoo.com>
2022-10-03 13:24:10 -0500

    experience update

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -56,13 +56,6 @@ const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
-                <h4>Node JS</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
                 <h4>Python</h4>
                 <small className="text-light">Basic</small>
               </div>
